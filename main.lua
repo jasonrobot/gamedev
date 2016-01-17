efb9d0d47be61c5ee4250496234ac033df66b19c
Jason Howell <jasonrobot64@gmail.com>
2016-01-16 17:49:50 -0800

    switched things around to use some different libraries

--- main.lua
+++ main.lua
@@ -1,64 +1,35 @@
 -- main.lua
 -- entry point for the Love2d game engine
-local sti = require "sti"
+local Camera = require "lib.hump.camera"
+local window = {}
+
+player = {}
+player.pos = {}
+player.pos.x = 200
+player.pos.y = 200
 
 function love.load()
-   love.window.setMode(640, 640)   
-
-   map = sti.new("tiled-02.lua")
-
-   -- create a new dynamic layer for sprites as layer 5
-   local layer = map:addCustomLayer("Sprites", 3)
-
-   -- he says: get the player spawn object
-   --  I'm guessing that the object in the map is just what we're using as a refrence to
-   --  place our fully dynamic entity on
-   local playerObject
-   for i, object in ipairs(map.objects) do
-      if object.name == "player" then
-	 playerObject = object
-	 break
-      end
-   end
-
-   --create the player object
-   local sprite = love.graphics.newImage("megaman.png")
-   layer.player = {
-      sprite = sprite,
-      x = playerObject.x,
-      y = playerObject.y,
-      ox = sprite:getWidth() / 2,
-      oy = sprite:getHeight() / 1.35,
-   }
-
-   --   layer.draw = function(self)
-   function layer:draw()
-      love.graphics.draw(
-	 self.player.sprite,
-	 math.floor(self.player.x),
-	 math.floor(self.player.y),
-	 0,
-	 1,
-	 1,
-	 self.player.ox,
-	 self.player.oy
-      )
-
-      --draw a point at the location, just to use as a refrence or something
-      --to make sure our draw method isnt fucked
-      love.graphics.setPointSize(5)
-      love.graphics.points(math.floor(self.player.x), math.floor(self.player.y))
-   end
-
-   map:removeLayer("player")
+   cam = Camera(player.pos.x, player.pos.y)
+   map = love.graphics.newImage("assets/blue.png")
+   
 end
 
 function love.update(dt)
-   map:update(dt)
+   player.pos.x = player.pos.x + 1
+--   print(cam.x, cam.y)
+   local dx, dy = player.pos.x - cam.x, player.pos.y - cam.y
+   print(dx, dy)
+   cam:move(dx/2, dy/2)
    
 end
 
 function love.draw()
-   map:draw()
+   --begin camera
+   cam:attach()
+   
+   love.graphics.draw(map, 0, 0)
+
+   cam:detach()
+   -- end camera
    
 end
