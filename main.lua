b4b61a3b851d49007ecca444e629fd439893b51e
Jason Howell <Jason Howell>
2016-01-31 10:54:08 -0800

    fresh off of master, starting gamestate implementation

--- main.lua
+++ main.lua
@@ -16,42 +16,42 @@ function love.load ()
 
    -- actor = Actor.new(200, 200)
    -- anotherActor = Actor.new(666, 666)
-   actor = Actor(200, 200, "assets/placeholder.png")
+   -- actor = Actor(200, 200, "assets/placeholder.png")
 
-   anotherActor = Actor(666, 666, "assets/placeholder.png")
+   -- anotherActor = Actor(666, 666, "assets/placeholder.png")
 
-   cam = Camera(actor.ps:center())
-   map = love.graphics.newImage("assets/blue.png")
+   -- cam = Camera(actor.ps:center())
+   -- map = love.graphics.newImage("assets/blue.png")
 
-   signalRegistry = Signal.new()
+   -- signalRegistry = Signal.new()
    
 end
 
 function love.update (dt)
-   -- Handle input here
+   -- -- Handle input here
 
-   -- Call the controll handler to fire input events
-   --   This will move all the controlled actors
-   -- Call the AI routine to play a turn for NPC actors
-   -- Call the camera routine to update the camera location
+   -- -- Call the controll handler to fire input events
+   -- --   This will move all the controlled actors
+   -- -- Call the AI routine to play a turn for NPC actors
+   -- -- Call the camera routine to update the camera location
 
-   actor:moveBy(1, 0)
-   --   print(cam.x, cam.y)
-   local ax, ay = actor.ps:center()
-   local dx, dy = ax - cam.x, ay - cam.y
+   -- actor:moveBy(1, 0)
+   -- --   print(cam.x, cam.y)
+   -- local ax, ay = actor.ps:center()
+   -- local dx, dy = ax - cam.x, ay - cam.y
 
-   cam:move(dx/2, dy/2)
+   -- cam:move(dx/2, dy/2)
    
 end
 
 function love.draw ()
-   --begin camera
-   cam:attach()
+   -- --begin camera
+   -- cam:attach()
    
-   love.graphics.draw(map, 0, 0)
-   actor:draw()
+   -- love.graphics.draw(map, 0, 0)
+   -- actor:draw()
 
-   cam:detach()
-   -- end camera
+   -- cam:detach()
+   -- -- end camera
    
 end
