2bae327d8df765422aedd11484094cba05f77ad2
Jason Howell <Jason Howell>
2016-02-14 00:43:32 -0800

    added hump vector support in objects and controllers

--- src/FollowerController.lua
+++ src/FollowerController.lua
@@ -21,25 +21,35 @@ function FollowerController:update(dt)
    local distance = math.sqrt(math.abs( (x - tx)^2 + (y - ty)^2 ))
    if distance > followDistance then
       --find the direction to the target
-      if math.abs(tx - x) > distance / 2 then
+      if math.abs(tx - x) > 5 then
 	 if tx > x then
-	    self.object.dx = self.object.dx + self.object.dMax
+	    self.object.intentions.right = true
+	    self.object.intentions.left = false
 	 else
-	    self.object.dx = self.object.dx - self.object.dMax	 
+	    self.object.intentions.left = true
+	    self.object.intentions.right = false
 	 end
+      else
+	 self.object.intentions.left = false
+	 self.object.intentions.right = false
       end
-      if math.abs(ty - y) > distance / 2 then      
+      if math.abs(ty - y) > 5 then
 	 if ty > y then
-	    self.object.dy = self.object.dy + self.object.dMax
+	    self.object.intentions.down = true
+	    self.object.intentions.up = false	    
 	 else
-	    self.object.dy = self.object.dy - self.object.dMax
+	    self.object.intentions.up = true
+	    self.object.intentions.down = false
 	 end
+      else
+	 self.object.intentions.up = false
+	 self.object.intentions.down = false
       end
-      --call the movement functions on self.object that move towards target
---      print(self.object.dx, self.object.dy)
    else
-      self.object.dx = 0
-      self.object.dy = 0
+      self.object.intentions.up = false
+      self.object.intentions.down = false
+      self.object.intentions.left = false
+      self.object.intentions.right = false
    end
    
    self.object:update(dt)
