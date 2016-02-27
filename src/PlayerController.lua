b0ca9e219b2cec619ce4fbd92485172189215997
Jason Howell <jasonrobot64@gmail.com>
2016-02-27 12:05:18 -0800

    this isnt working

--- src/PlayerController.lua
+++ src/PlayerController.lua
@@ -1,15 +1,27 @@
 ---- PlayerKeymap.lua
 --- This is really the controller for an actor class that uses player input
 --- Depends on Object.lua
+-- global Signal = require 'hump.signal'
 local Controller = require 'Controller'
 
 local PlayerController = setmetatable({}, Controller)
 PlayerController.__index = PlayerController
 
 local function new(newObject)
-   local t = setmetatable({object = newObject}, PlayerController)
+   local t = {}
+   t.object = newObject
+   local x, y = newObject.getCenter()
+   local w, h = love.window.getMode()
+   x = x - w/2
+   y = y - h/2
+   -- FIXME each object or entity should just have one of these shapes on hand at all times
+   local shape = HC.rectangle(x, y, w, h)
+
+   t.entityVision = HC.rectangle()
+   t = setmetatable(t, PlayerController)
    -- do we really want to do this this way?
    t:registerCallbacks()
+   
    return t
 end
 
@@ -29,5 +41,7 @@ function PlayerController:registerCallbacks()
    Signal.register('left_released', function() self.object.intentions.left = false end)
    Signal.register('right_released', function() self.object.intentions.right = false end)
 end
+
+
 
 return setmetatable({}, {__call = function(_,...) return new(...) end})
